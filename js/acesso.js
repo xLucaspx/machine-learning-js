bac9eec149c7318c07ccde3f4d41b87fa4950de0
Lucas da Paz <xlucaspx@gmail.com>
2024-07-16 21:02:14 -0300

    buscando arquivo por fetch ao inves de form

--- js/acesso.js
+++ js/acesso.js
@@ -6,49 +6,54 @@ const reader = new FileReader();
 
 const network = new brain.NeuralNetwork();
 
-form.onsubmit = async (event) => {
-	event.preventDefault();
+// form.onsubmit = async (event) => {
+// 	event.preventDefault();
 
-	if (fileInput.files.length === 0) {
-		console.error("Nenhum arquivo selecionado!");
-		return;
+// 	if (fileInput.files.length === 0) {
+// 		console.error("Nenhum arquivo selecionado!");
+// 		return;
+// 	}
+
+// 	const file = fileInput.files[0];
+
+// 	if (!file.type === "text/csv") {
+// 		console.error("Formato de arquivo inválido!");
+// 		return;
+// 	}
+
+// 	// TODO: safe file name
+
+console.log("Arquivo acesso.csv")
+
+const file = await fetch("../files/acesso.csv");
+const blob = await file.blob();
+
+reader.readAsText(blob);
+reader.onloadend = () => {
+	const data = reader.result;
+	const arr = data.split('\r\n');
+	const header = arr.shift().split(',');
+
+	const trainData = getTrainData(header, arr.slice(-10)); // últimas 10 linhas são tidas como verdadeiras
+
+	network.train(trainData);
+
+	const toBeTested = getDataToBeTested(header, arr.slice(0, - 10));
+	let verdadeiros = 0;
+
+	for (const line of toBeTested) {
+		const output = line.output;
+		const result = network.run(line.input);
+
+		if (Math.round(result.comprou) == output) verdadeiros++
 	}
 
-	const file = fileInput.files[0];
+	const totalElementos = toBeTested.length;
+	console.log("Total de elementos: ", totalElementos);
+	console.log("Verdadeiros: ", verdadeiros);
+	console.log(`Taxa de acertos: ${(100 * verdadeiros) / totalElementos}%`);
+}
 
-	if (!file.type === "text/csv") {
-		console.error("Formato de arquivo inválido!");
-		return;
-	}
-
-	// TODO: safe file name
-
-	reader.readAsText(file);
-	reader.onloadend = () => {
-		const data = reader.result;
-		const arr = data.split('\r\n');
-		const header = arr.shift().split(',');
-
-		const trainData = getTrainData(header, arr.slice(-10)); // últimas 10 linhas são tidas como verdadeiras
-
-		network.train(trainData);
-
-		const toBeTested = getDataToBeTested(header, arr.slice(0, - 10));
-		let verdadeiros = 0;
-
-		for (const line of toBeTested) {
-			const output = line.output;
-			const result = network.run(line.input);
-
-			if (Math.round(result.comprou) == output) verdadeiros++
-		}
-
-		const totalElementos = toBeTested.length;
-		console.log("Total de elementos: ", totalElementos);
-		console.log("Verdadeiros: ", verdadeiros);
-		console.log(`Taxa de acertos: ${(100 * verdadeiros) / totalElementos}%`);
-	};
-};
 
 function getTrainData(header, inputs) {
 	const dataArr = [];
